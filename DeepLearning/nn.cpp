052540181024117b61c974334f2b6fc540f1d9cc
Kieren James Hussey <khussey45@gmail.com>
2023-07-22 10:27:17 -0400

    Where art thou compiler

---
+++ DeepLearning/nn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+//      int input = [1,2,3,4];
+
+//      int weight = [1,2,3,4];
+
+//     bias = 2;
+
+//    float output = input * weight + bias;
+
+      std::cout << output << std::endl;
+    return 0;
+}
