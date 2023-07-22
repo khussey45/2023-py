f96484d91a799387c2a1cfcb00f0f212ace241a1
Kieren James Hussey <khussey45@gmail.com>
2023-07-21 22:27:42 -0400

    added deep learning folder - sentdex series

---
+++ beginner/lists.py
@@ -0,0 +1,22 @@
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# motorcycles.append('ducati')
+# print(motorcycles)
+
+motorcycles = []
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+# Inserting a new element at any position with insert 
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+# Delete items with the del statement 
+del motorcycles[0]
+print(motorcycles)
+
