ac9609a29f2acdd3528e083a9b11eb5e3fe9087c
Kieren James Hussey <khussey45@gmail.com>
2023-07-22 10:39:59 -0400

    c++ nn vs py nn

--- DeepLearning/nn.cpp
+++ DeepLearning/nn.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
 int main() {
-  
-//      int input = [1,2,3,4];
+    int input[] = {1, 2, 3, 4};
+    float weight[] = {1.5, 2.5, 3.5, 4.5};
+    int bias = 2;
+    float output = 0;
 
-//      int weight = [1,2,3,4];
+    // Perform the weighted sum
+    for (int i = 0; i < sizeof(input) / sizeof(input[0]); i++) {
+        output += input[i] * weight[i];
+    }
 
-//     bias = 2;
+    // Add the bias
+    output += bias;
 
-//    float output = input * weight + bias;
-
-      std::cout << output << std::endl;
+    std::cout << output << std::endl;
     return 0;
 }
---
+++ DeepLearning/nn1.py
@@ -0,0 +1,7 @@
+inputs = [1, 2, 3, 4]
+weights = [1.5, 2.5, 3.5, 4.5]
+bias = 2
+
+output = inputs[0]*weights[0]+ inputs[1]*weights[1]+ inputs[2]*weights[2]+ inputs[3]*weights[3] + bias
+
+print(output)
