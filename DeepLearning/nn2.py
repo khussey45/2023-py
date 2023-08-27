910f1d09f0a4c289875578627999589571d8cf34
Kieren James Hussey <khussey45@gmail.com>
2023-08-26 22:33:48 -0400

    nn2 added

---
+++ DeepLearning/nn2.py
@@ -0,0 +1,13 @@
+# Inputs
+inputs = [1, 2, 3, 4]
+
+# Weights
+weights = [1.5, 2.5, 3.5, 4.5]
+
+# Bias
+bias = 3
+
+
+# Outputs
+output = inputs[0]*weights[0]+ inputs[1]*weights[1]+ inputs[2]*weights[2]+ inputs[3]*weights[3] + bias
+print(output)
