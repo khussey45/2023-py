a9d146c81e5504d75e45f3d1ddf19b3e43f8f133
Kieren James Hussey <khussey45@gmail.com>
2023-08-26 23:04:47 -0400

    messing with single layer nn

--- DeepLearning/nn.py
+++ DeepLearning/nn.py
@@ -1,4 +1,4 @@
-inputs = [1, 2, 3, 2.5]
+inputs = [1, 2, 3, 4]
 
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26,-0.5]
@@ -12,4 +12,5 @@ output = [inputs[0]*weights1[0]+ inputs[1]*weights1[1]+ inputs[2]*weights1[2]+ i
           inputs[0]*weights2[0]+ inputs[1]*weights2[1]+ inputs[2]*weights2[2]+ inputs[3]*weights2[3] + bias2,
           inputs[0]*weights3[0]+ inputs[1]*weights3[1]+ inputs[2]*weights3[2]+ inputs[3]*weights3[3] + bias3
           ]
-print(output)  
+output_rounded = [round(num, 2) for num in output]
+print(output_rounded)
