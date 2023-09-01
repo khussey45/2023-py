fa3c05ae4fd6118e6db8ced5dbc52db50e1e6ee9
Kieren James Hussey <khussey45@gmail.com>
2023-09-01 18:04:13 -0400

    numpy

---
+++ DeepLearning/test.py
@@ -0,0 +1,10 @@
+# Lets practice some basic python commands using Numpy
+
+# Import the numpy library
+import numpy as np
+
+# Create a 1D array of numbers from 0 to 9
+a = np.arange(10) 
+
+# Print the array
+print(a)  
